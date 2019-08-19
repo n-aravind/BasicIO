0b1ebdcc406309d788ead8928a51802c2379b31e
Aravind <aravindnareddy@gmail.com>
2019-08-18 17:00:19 -0700

    updated as per comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class Main {
     // TODO GP comment: remove the throws IOException and InterruptedException
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyy HH:mm:ss");
         BufferedWriter writer = null;
 
@@ -15,11 +15,24 @@ public class Main {
                 writer = new BufferedWriter(new FileWriter("datefile.html"));
                 writer.write("<html><body><h1>" + dateFormat.format(new Date()) + "</h1></body></html>");
 
-            } finally {
-                if (writer != null)
-                    writer.close();
             }
-            Thread.sleep(1000);
+            catch (Exception e){
+                System.out.println("Failed during File Write Operation");
+            } finally{
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("Cannot close file");
+                    }
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
     }
